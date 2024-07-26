7fccdc5794aa19a791274db5ed0bfffb0e6d0767
Niyah <Rosiedozie101@gmail.com>
2024-07-26 10:13:27 -0400

    Initial Commit

---
+++ InteractiveApp/InteractiveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveAppApp.swift
+//  InteractiveApp
+//
+//  Created by Niyah Murphy on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
